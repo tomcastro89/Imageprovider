364be872446fc39db8870451dc1301c0e6e58b7f
Tommaso Castrovillari <tcastrovillari@itemis.de>
2019-07-16 15:05:10 +0200

    Step 10: Make the Endpoint accessible from Frontend by adding the CrossOrigin Annotation

--- src/main/java/com/itemis/demo/imageprovider/ImageproviderRestController.java
+++ src/main/java/com/itemis/demo/imageprovider/ImageproviderRestController.java
@@ -1,5 +1,6 @@
 package com.itemis.demo.imageprovider;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import java.util.List;
 @RestController
 public class ImageproviderRestController {
     @GetMapping(value = "/images")
+    @CrossOrigin(origins = "http://localhost:3000")
     public List<Image> getImages(){
         List<Image> images = new ArrayList<>();
         images.add(new Image(
