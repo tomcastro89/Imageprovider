97a01ffa9639ddc3a5dad2ef542bb3f3a25010e6
Tommaso Castrovillari <tcastrovillari@itemis.de>
2019-07-16 14:45:40 +0200

    Step 1: Create a new Spring Boot Project

---
+++ src/main/java/com/itemis/demo/imageprovider/ImageproviderApplication.java
@@ -0,0 +1,13 @@
+package com.itemis.demo.imageprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageproviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageproviderApplication.class, args);
+    }
+
+}
