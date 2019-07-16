5c75c71292d8483ba872b4f3c65a9adea01c47cb
Tommaso Castrovillari <tcastrovillari@itemis.de>
2019-07-16 14:50:30 +0200

    Step 4: Declare that the ImageproviderRestController is a Spring RestController with an available “/images” endpoint

--- src/main/java/com/itemis/demo/imageprovider/ImageproviderRestController.java
+++ src/main/java/com/itemis/demo/imageprovider/ImageproviderRestController.java
@@ -1,4 +1,12 @@
 package com.itemis.demo.imageprovider;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ImageproviderRestController {
+    @GetMapping(value = "/images")
+    public void getImages(){
+        return;
+    }
 }
