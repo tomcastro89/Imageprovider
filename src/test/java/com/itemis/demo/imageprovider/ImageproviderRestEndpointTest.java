bd248855005c24bef16a10a3178917e54fef2ed5
Tommaso Castrovillari <tcastrovillari@itemis.de>
2019-07-16 14:54:02 +0200

    Step 5: Write a Test that describes that our endpoint should return content of type JSON

--- src/test/java/com/itemis/demo/imageprovider/ImageproviderRestEndpointTest.java
+++ src/test/java/com/itemis/demo/imageprovider/ImageproviderRestEndpointTest.java
@@ -4,10 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -19,5 +21,10 @@ public class ImageproviderRestEndpointTest {
     @Test
     public void imagesEndpointIsAvailable() throws Exception {
         mockMvc.perform(get("/images")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void imagesEndpointReturnsContentOfTypeJSON() throws Exception {
+        mockMvc.perform(get("/images")).andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
     }
 }
